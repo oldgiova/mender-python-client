6bc277ce9173806655ed5f87f2d836ef6a612ad0
Marcin Żyrkowski <marcin.zyrkowski@tronel.pl>
2021-04-12 10:01:07 +0000

    the hardcoded entries removed

--- src/mender/remoteterminal/remoteterminal.py
+++ src/mender/remoteterminal/remoteterminal.py
@@ -34,34 +34,36 @@ class RemoteTerminal:
 
     def __init__(self):
         log.debug("RemoteTerminal initialized")
-        self._ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-        # @fixme: check if the file exists
-        self._ssl_context.load_verify_locations("/etc/mender/server.crt")
 
         self._client = None
         self._sid = None
         self._ws_connected = False
         self._hello_failed = False
+        self._context = None
+        #started by a protocol msg "new", ended (set back to False) after a protocol msg "end" @todo
+        self._session_started = False
 
     async def ws_connect(self):
-        # @fixme change to a proper server name
-        uri = "wss://docker.mender.io/api/devices/v1/deviceconnect/connect"
+        # from context we receive sth like: "ServerURL": "https://docker.mender.io"
+        # we need replace the protcol and API entry point to achive like: "wss://docker.mender.io/api/devices/v1/deviceconnect/connect"
+        uri = self._context.config.ServerURL.replace("https", "wss") + "/api/devices/v1/deviceconnect/connect"
         try:
             self._client = await websockets.connect(
                 uri, ssl=self._ssl_context, extra_headers=self._ext_headers)
+            self._ws_connected = True
             log.debug(f'connected: {self._client}')
         except Exception as inst:
             log.debug(f'ws_connect: {type(inst)}')
             log.debug(f'ws_connect: {inst}')
 
-    async def ws_send_termina_stdout_to_backend(self):
+    async def ws_send_terminal_stdout_to_backend(self):
         # wait for connection in another coroutine
-        while not self._ws_connected and not self._hello_failed:
+        while not self._session_started and not self._hello_failed:
             await asyncio.sleep(1)
         if self._hello_failed:
-            log.debug('leaving send_stdout2')
+            log.debug('leaving ws_send_terminal_stdout_to_backend')
             return -1
-        log.debug('send_stdout GO!')
+        log.debug('going into clnt->bcknd loop')
         while True:
             try:
                 await asyncio.sleep(1)
@@ -69,14 +71,15 @@ class RemoteTerminal:
                 resp_header = {'proto': 1, 'typ': 'shell', 'sid': self._sid}
                 resp_props = {'status': 1}
                 response = {'hdr': resp_header, 'props': resp_props, 'body': data}
-                log.debug(f'resp: {response}')
+                #log.debug(f'resp: {response}')
                 await self._client.send(msgpack.packb(response))
-                log.debug('wyslalem')
+                log.debug('data sent')
 
                 # @fixme try another approach: instead of making the fd non-blocking run in a separate asyncio.to_thread (same fixme is down there)
             except Exception as ex_instance:
-                log.debug(f'send_stdout: {type(ex_instance)}')
-                log.debug(f'send_stdout: {ex_instance}')
+                pass # @fixme those execption are catched all the time due to non-blocking, commented out for keeping the output tidier for testing
+                #log.debug(f'send_stdout: {type(ex_instance)}')
+                #log.debug(f'send_stdout: {ex_instance}')
 
     async def ws_read_from_backend_write_to_terminal(self):
 #        while self._client is None:
@@ -86,7 +89,7 @@ class RemoteTerminal:
             self._hello_failed = True
             log.debug('hello failed')
             return -1
-        log.debug('hello GO!')
+        log.debug('wss connectied, going into bcknd->clnt loop')
         try:
             while True:
                 log.debug('about to waiting for msg from backend')
@@ -96,7 +99,7 @@ class RemoteTerminal:
                 hdr = msg['hdr']
                 if hdr['typ'] == 'new':
                     self._sid = hdr['sid']
-                    self._ws_connected = True
+                    self._session_started = True
                 if hdr['typ'] == 'shell' :
                     log.debug('waiting for _master for writing')
                     _, ready, _, = select.select([], [self._master], [])
@@ -114,7 +117,7 @@ class RemoteTerminal:
             log.debug(f'hello: {inst}')
 
     async def gather(self):
-        await asyncio.gather(self.ws_read_from_backend_write_to_terminal(), self.ws_send_termina_stdout_to_backend())
+        await asyncio.gather(self.ws_read_from_backend_write_to_terminal(), self.ws_send_terminal_stdout_to_backend())
 
     def thread_f(self):
         try:
@@ -125,7 +128,13 @@ class RemoteTerminal:
             log.debug(f'in Run: {inst}')
 
     def run(self, context):
+        self._context = context
         if context.config.RemoteTerminal and context.authorized and not self._ws_connected:
+            log.debug(f'_ws_connected={self._ws_connected}')
+            self._ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+            # @fixme: check if the file exists
+            self._ssl_context.load_verify_locations(context.config.ServerCertificate)
+
             # the JWT should already be acquired as we supposed to be in AuthorizedState
             self._ext_headers = {
                 'Authorization': 'Bearer ' + context.JWT
