01d44ecea141f6a728f6c027d89b68563f223059
Grzegorz Janusz <grzegorz.janusz@tronel.pl>
2021-04-21 17:29:12 +0200

    code cleanup

--- src/mender/remoteterminal/remoteterminal.py
+++ src/mender/remoteterminal/remoteterminal.py
@@ -20,7 +20,7 @@ import select
 import ssl
 import subprocess
 import threading
-import signal
+#import signal
 
 import msgpack
 import websockets
@@ -66,42 +66,13 @@ class RemoteTerminal:
             log.debug(f'ws_connect: {type(inst)}')
             log.debug(f'ws_connect: {inst}')
 
-    async def ws_send_terminal_stdout_to_backend(self):
-        # wait for connection in another coroutine
-        # while not self._session_started and not self._hello_failed:
-        #    await asyncio.sleep(1)
-        if self._hello_failed:
-            log.debug('leaving ws_send_terminal_stdout_to_backend')
-            return -1
-        log.debug('going into clnt->bcknd loop')
-        while True:
-            try:
-                # await asyncio.sleep(1)
-                data = os.read(self._master, 102400)
-                resp_header = {'proto': 1, 'typ': 'shell', 'sid': self._sid}
-                resp_props = {'status': 1}
-                response = {'hdr': resp_header,
-                            'props': resp_props, 'body': data}
-                #log.debug(f'resp: {response}')
-                await self._client.send(msgpack.packb(response, use_bin_type=True))
-                log.debug('data sent')
-            except Exception as ex_instance:
-                pass    # @fixme those execption are catched all the time due to non-blocking,
-                # commented out for keeping the output tidier for testing
-                #log.debug(f'send_stdout: {type(ex_instance)}')
-                #log.debug(f'send_stdout: {ex_instance}')
-
-    async def ws_read_from_backend_write_to_terminal(self):
-        #        while self._client is None:
-     #           await asyncio.sleep(1)
-        log.debug(locals())
-        log.debug(f'self client {self._client}')
+    async def ws_listener_background(self):
         await self.ws_connect()
         if self._client is None:
             self._hello_failed = True
-            log.debug('hello failed')
+            log.debug('Websocket connection failed')
             return -1
-        log.debug('wss connected, going into bcknd->clnt loop')
+        log.debug('Websocket connected')
         try:
             while True:
                 log.debug('about to waiting for msg from backend')
@@ -112,32 +83,59 @@ class RemoteTerminal:
                     self._sid = hdr['sid']
                     self._session_started = True
                     self._open_terminal()
-                    self.thread_transmit()
-                    #self.thread_read.start()
+                    self._thread_transmit()
+                    # self.thread_read.start()
                 if hdr['typ'] == 'shell':
-                    log.debug('waiting for _master for writing')
-                    _, ready, _, = select.select([], [self._master], [])
-                    for stream in ready:
-                        log.debug('stream in _master READY for WRITING')
-                        try:
-                            os.write(stream, msg['body'])
-                            # os.write(stream, 'ls\n'.encode('utf-8'))
-                        except Exception as ex_instance:
-                            log.error(
-                                f'while writing to master: {type(ex_instance)}')
-                            log.error(
-                                f'while writing to master: {ex_instance}')
+                    self.ws_read_from_backend_write_to_terminal(msg)
                 if hdr['typ'] == 'stop':
                     log.debug('close terminal msg')
-                    #os.killpg(os.getpgid(self._shell.pid), signal.SIGTERM)
+                    # os.killpg(os.getpgid(self._shell.pid), signal.SIGTERM)
                     self._shell.kill()
                     self._master = None
                     self._slave = None
-                    #self.thread_send.kill()
+                    # self.thread_send.join()
 
         except Exception as inst:
             log.error(f'hello: {type(inst)}')
             log.error(f'hello: {inst}')
+
+    async def ws_send_terminal_stdout_to_backend(self):
+        if self._hello_failed:
+            log.debug('leaving ws_send_terminal_stdout_to_backend')
+            return -1
+        log.debug('going into clnt->bcknd loop')
+        while True:
+            try:
+                data = os.read(self._master, 102400)
+                resp_header = {'proto': 1, 'typ': 'shell', 'sid': self._sid}
+                resp_props = {'status': 1}
+                response = {'hdr': resp_header,
+                            'props': resp_props, 'body': data}
+                # log.debug(f'resp: {response}')
+                await self._client.send(msgpack.packb(response, use_bin_type=True))
+                log.debug('data sent')
+            except Exception as ex_instance:
+                pass    # @fixme those execption are catched all the time due to non-blocking,
+                # commented out for keeping the output tidier for testing
+                # log.debug(f'send_stdout: {type(ex_instance)}')
+                # log.debug(f'send_stdout: {ex_instance}')
+
+    def ws_read_from_backend_write_to_terminal(self, msg):
+        log.debug('waiting for _master for writing')
+        _, ready, _, = select.select([], [self._master], [])
+        for stream in ready:
+            log.debug('stream in _master READY for WRITING')
+            try:
+                os.write(stream, msg['body'])
+                # os.write(stream, 'ls\n'.encode('utf-8'))
+            except Exception as ex_instance:
+                log.error(
+                    f'while writing to master: {type(ex_instance)}')
+                log.error(
+                    f'while writing to master: {ex_instance}')
+            except Exception as inst:
+                log.error(f'hello: {type(inst)}')
+                log.error(f'hello: {inst}')
 
     def thread_f_recieve(self):
         try:
@@ -153,15 +151,27 @@ class RemoteTerminal:
             log.error(f'in thread_f_transmit: {type(inst)}')
             log.error(f'in thread_f_transmit: {inst}')
 
-    def thread_recieve(self):
+    def thread_f_ws(self):
+        try:
+            asyncio.run(self.ws_listener_background())
+        except Exception as inst:
+            log.error(f'in thread_f_transmit: {type(inst)}')
+            log.error(f'in thread_f_transmit: {inst}')
+
+    def _thread_recieve(self):
         log.debug('about to start read thread')
         thread_read = threading.Thread(target=self.thread_f_recieve)
         thread_read.start()
 
-    def thread_transmit(self):
+    def _thread_transmit(self):
         log.debug('about to start send thread')
         thread_send = threading.Thread(target=self.thread_f_transmit)
         thread_send.start()
+
+    def _background_thread_ws(self):
+        log.debug('about to start naver ending thread, websocket connection')
+        thread_ws = threading.Thread(target=self.thread_f_ws)
+        thread_ws.start()
 
     def _open_terminal(self):
         user = 'nobody'
@@ -196,8 +206,5 @@ class RemoteTerminal:
                 'Authorization': 'Bearer ' + context.JWT
             }
 
-            # @fixme the following part needs to be moved
-            # "after the connection has been established"
-
-            self.thread_recieve()
+            self._background_thread_ws()
             log.debug("i've just invoked the websocket thread")
