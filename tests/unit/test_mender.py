4ef8295fed0f4674e1ce65bd0f804e133333861d
Ole Petter <ole.orhagen@northern.tech>
2021-09-17 08:25:15 +0200

    Set the base version to 1.1.x in _version.py
    Changelog: None
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- tests/unit/test_mender.py
+++ tests/unit/test_mender.py
@@ -90,7 +90,7 @@ def test_run_bootstrap(args, monkeypatch):
 
 def test_run_version(capsys):
     main.run_version({})
-    assert "version: master" in capsys.readouterr().out
+    assert "version: 1.1.x" in capsys.readouterr().out
 
 
 def test_report(args, ctx, caplog, tmpdir, monkeypatch):
